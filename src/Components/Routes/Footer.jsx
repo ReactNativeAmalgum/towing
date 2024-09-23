7ea23193ed9475724b367b4fcdff96370ea8d377
ReactNativeAmalgum <137068385+ReactNativeAmalgum@users.noreply.github.com>
2024-09-23 17:25:16 +0530

    footer color change

--- src/Components/Routes/Footer.jsx
+++ src/Components/Routes/Footer.jsx
@@ -15,11 +15,11 @@ export default function Footer() {
                         data-animation="fadeInUp"
                     >
                         <a href="./" className="logo">
-                            <img src="images/logo.png" alt="" />
-                            <span className="logo_text">
-                                <span className="highlight">24/7 TOWY</span>
-                                <span>Towing Services</span>
-                            </span>
+                            <div style={{ width: 80 }}>
+                                <img src="http://moderntowing.in/wp-content/uploads/2023/01/IMG-20230103-WA00001.jpg" alt="" />
+
+                            </div>
+
                         </a>
                         <p>
                             Shank duis pancetta kevin ullamco tempor short loin pig lorem officia
@@ -42,23 +42,23 @@ export default function Footer() {
                             <div className="widget widget_pages">
                                 <h3 className="widget-title">
                                     Useful
-                                    <strong>Links</strong>
+                                    <strong> Links</strong>
                                 </h3>
                                 <hr className="divider_30_3 zebra_bg divider_left" />
                                 <div className="row columns_padding_0 columns_margin_0">
                                     <div className="col-xs-6">
                                         <ul className="greylinks">
                                             <li>
-                                                <MdKeyboardArrowRight style={{color:'#ff0000'}} />
+                                                <MdKeyboardArrowRight style={{ color: '#ff0000' }} />
                                                 <a href="about.html">About</a>
                                             </li>
                                             <li>
-                                                <MdKeyboardArrowRight style={{color:'#ff0000'}} />
+                                                <MdKeyboardArrowRight style={{ color: '#ff0000' }} />
 
                                                 <a href="services.html">Services</a>
                                             </li>
                                             <li>
-                                                <MdKeyboardArrowRight style={{color:'#ff0000'}} />
+                                                <MdKeyboardArrowRight style={{ color: '#ff0000' }} />
 
                                                 <a href="gallery-tile.html">Gallery</a>
                                             </li>
@@ -69,7 +69,7 @@ export default function Footer() {
 
 
                                             <li>
-                                            <MdKeyboardArrowRight style={{color:'#ff0000'}} />
+                                                <MdKeyboardArrowRight style={{ color: '#ff0000' }} />
 
                                                 <a href="contact.html">Contacts</a>
                                             </li>
@@ -86,30 +86,30 @@ export default function Footer() {
                         <div className="widget widget_text topmargin_15">
                             <h3 className="widget-title">
                                 Get in
-                                <strong>Touch</strong>
+                                <strong> Touch</strong>
                             </h3>
                             <hr className="divider_30_3 zebra_bg divider_left" />
                             <div className="media small-teaser custom-small-teaser">
-                                    <i className="fa fa-map-marker highlight fontsize_18" />
-                                    <IoLocationSharp
-                                    style={{color:'#ff0000', marginRight:20}} />
+                                <i className="fa fa-map-marker highlight fontsize_18" />
+                                <IoLocationSharp
+                                    style={{ color: '#ff0000', marginRight: 20 }} />
                                 <div className="media-body">2551 Alfred Drive Brooklyn, NY</div>
                             </div>
                             <div className="media small-teaser custom-small-teaser">
-                                    <i className="fa fa-envelope highlight fontsize_18" />
+                                <i className="fa fa-envelope highlight fontsize_18" />
                                 <div className="media-body greylinks">
-                                <MdEmail style={{color:'#ff0000', marginRight:20}} />
+                                    <MdEmail style={{ color: '#ff0000', marginRight: 20 }} />
                                     <a href="mailto:your@mail.com">support@towy.com</a>
                                 </div>
                             </div>
                             <div className="media small-teaser custom-small-teaser">
-                                    <i className="fa fa-phone highlight fontsize_18" />
-                                    <FaPhone style={{color:'#ff0000', marginRight:20}} />
+                                <i className="fa fa-phone highlight fontsize_18" />
+                                <FaPhone style={{ color: '#ff0000', marginRight: 20 }} />
                                 <div className="media-body">718-250-4467</div>
                             </div>
                             <div className="media small-teaser custom-small-teaser">
-                                    <i className="fa fa-clock-o highlight fontsize_18" />
-                                    <FaClock style={{color:'#ff0000', marginRight:20}} />
+                                <i className="fa fa-clock-o highlight fontsize_18" />
+                                <FaClock style={{ color: '#ff0000', marginRight: 20 }} />
                                 <div className="media-body">24 hours a day, 7 days a week</div>
                             </div>
                         </div>
@@ -118,7 +118,7 @@ export default function Footer() {
                         className="col-md-3 col-sm-12 to_animate animated fadeInUp"
                         data-animation="fadeInUp"
                     >
-                        <div className="topmargin_15">
+                        {/* <div className="topmargin_15">
                             <div className="widget widget_twitter">
                                 <h3 className="widget-title">
                                     Latest
@@ -129,7 +129,7 @@ export default function Footer() {
                                     <ul className="tweet_list" />
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
